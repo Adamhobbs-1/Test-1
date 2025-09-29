548e47408b5ad7bf30e3628cad23bf227135dc21
theTech66 <adamichael04@gmail.com>
2025-09-29 14:56:57 -0600

    init: hello script and git hygiene

---
+++ hello.py
@@ -0,0 +1,8 @@
+from rich.console import Console
+import sys, platform
+
+c = Console()
+c.rule("[bold]Hello from venv")
+c.print(f"Python: {sys.version.split()[0]}")
+c.print(f"Executable: {sys.executable}")
+c.print(f"OS: {platform.system()} {platform.release()}")
