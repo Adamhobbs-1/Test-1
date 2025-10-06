3411346e1302a790c47503d2a1e9c9d2f13b2bb0
Adamhobbs-1 <adamichael04@gmail.com>
2025-10-06 09:17:24 -0600

    hello: this is just a test (#2)

--- hello.py
+++ hello.py
@@ -8,3 +8,6 @@ c.print(f"Executable: {sys.executable}")
 c.print(f"OS: {platform.system()} {platform.release()}")
 c.rule("[bold]Goodbye from venv")
 c.print("Goodbye from venv")
+
+variable = 5
+c.print(f"Variable value: {variable}")
